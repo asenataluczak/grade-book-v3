9c195a8d70b3209a11344d6ff73080ff35355b73
asenataluczak <asenata.luczak@gmail.com>
2023-05-19 19:40:07 +0200

    added apiSlice

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice.js";
 
 const store = configureStore({
-  reducer: { auth: authReducer },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  reducer: { auth: authReducer, [apiSlice.reducerPath]: apiSlice.reducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
